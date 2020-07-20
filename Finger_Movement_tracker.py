3d7c72476778fd4b7e0a93dec9d334f2f8c11d2c
Kiran Chavhan <54065701+kiranchavhan@users.noreply.github.com>
2020-07-20 12:43:26 +0530

    first comit
    only capture image

---
+++ Finger_Movement_tracker.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy
+cap = cv2.VideoCapture(0)
+while 1:
+    ret, img = cap.read()
+    cv2.imshow('img',img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
